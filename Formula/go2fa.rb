4f734554841867e0701da50ce99e5c4e94965292
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-05-23 13:56:23 +0100

    *grumble grumble*

--- Formula/go2fa.rb
+++ Formula/go2fa.rb
@@ -1,5 +1,5 @@
 class Go2fa < Formula
-  desc "Two-factor authentication on the command line"
+  desc "Two-factor authentication on the command-line"
   homepage "https://github.com/rsc/2fa"
   url "https://github.com/rsc/2fa/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "d8db6b9a714c9146a4b82fd65b54f9bdda3e58380bce393f45e1ef49e4e9bee5"
