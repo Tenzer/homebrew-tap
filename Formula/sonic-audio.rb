924c79e76bc100bf8137a612376537ace6156b76
Jeppe Fihl-Pearson <68696+Tenzer@users.noreply.github.com>
2021-05-23 21:05:23 +0000

    sonic-audio: add 20210523-0 bottle.

--- Formula/sonic-audio.rb
+++ Formula/sonic-audio.rb
@@ -6,6 +6,12 @@ class SonicAudio < Formula
   sha256 "469a66b6b54bb825efdc4cbaef1bbd45f87ddc6ce9f73ce7b1060f26b33ec6a9"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/Tenzer/homebrew-tap/releases/download/sonic-audio-20210523-0"
+    sha256 cellar: :any,                 catalina:     "89e00d9ac753897456e13b9617134d8352658cf2d88e4e8ac5449bddef89e8c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "04a9f03bc4f13b3ead503e0216ebe584eb1a00425c86c7e526367472306420ba"
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
