4faa689090a67384db64888b0e2461c6bcb1df6d
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-05-23 13:56:57 +0100

    GitHub is in fact accessible over HTTPS
    Closes #3.
    
    Signed-off-by: Jeppe Fihl-Pearson <68696+Tenzer@users.noreply.github.com>

--- Formula/quirky.rb
+++ Formula/quirky.rb
@@ -1,6 +1,6 @@
 class Quirky < Formula
   desc "QR code printer for your terminal"
-  homepage "http://github.com/Tenzer/quirky"
+  homepage "https://github.com/Tenzer/quirky"
   url "https://github.com/Tenzer/quirky/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "b70b7a98f36d874539dfdcbc8e0434293a1ee46779328945dbd6258ba8b92002"
   license "MIT"
