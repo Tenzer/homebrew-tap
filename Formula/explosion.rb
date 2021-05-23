6220eaf83cb0b6911925e75a2140ef82da786a90
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-05-23 13:26:40 +0100

    Add ARM64 macOS bottle for explosion

--- Formula/explosion.rb
+++ Formula/explosion.rb
@@ -7,8 +7,9 @@ class Explosion < Formula
 
   bottle do
     root_url "https://github.com/Tenzer/homebrew-tap/releases/download/explosion-1.1.2"
-    sha256 cellar: :any_skip_relocation, catalina:     "172ca40969bce62f88a6b6df73fbd354f7b8e869e6ed727b8dbf32bdf1e499dd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b71ea7230d6b92392c70a194491338c9533379f3ad5391e177b1d68a0fec5f5b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "05343e8f2b98023a66038a962e13c919683195518ca640533fe3710168f90269"
+    sha256 cellar: :any_skip_relocation, catalina:      "172ca40969bce62f88a6b6df73fbd354f7b8e869e6ed727b8dbf32bdf1e499dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b71ea7230d6b92392c70a194491338c9533379f3ad5391e177b1d68a0fec5f5b"
   end
 
   depends_on "go" => :build
