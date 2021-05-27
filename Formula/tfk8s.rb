2734f360abb86b2437c7c8e593bde7da1f1c17a1
Jeppe Fihl-Pearson <68696+Tenzer@users.noreply.github.com>
2021-05-27 21:28:31 +0000

    tfk8s: add 0.1.3 bottle.

--- Formula/tfk8s.rb
+++ Formula/tfk8s.rb
@@ -5,6 +5,12 @@ class Tfk8s < Formula
   sha256 "0dfb441441ab270e745e31830918efab2e51d38e9535c3293d797bf90c944822"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Tenzer/homebrew-tap/releases/download/tfk8s-0.1.3"
+    sha256 cellar: :any_skip_relocation, catalina:     "7f74f763151c7bcd6c498a608ebb71c71e96eb258976cfcaa56dc1b08bb6f768"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a1b652062d7c68a805d084d28456b9b292dfb634b38e2d68edf9de27d0d429c0"
+  end
+
   depends_on "go" => :build
 
   def install
