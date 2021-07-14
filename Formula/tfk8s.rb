a36d7253d5a8a60f9d7ffe2f41c3c2bda1870279
Jeppe Fihl-Pearson <68696+Tenzer@users.noreply.github.com>
2021-07-14 10:54:18 +0000

    tfk8s: update 0.1.5 bottle.

--- Formula/tfk8s.rb
+++ Formula/tfk8s.rb
@@ -6,10 +6,9 @@ class Tfk8s < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/Tenzer/homebrew-tap/releases/download/tfk8s-0.1.4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "c58a9c72f98f9bb38a6fe2624cc8d26da2249fc4eda3025c55424782d9a47faa"
-    sha256 cellar: :any_skip_relocation, catalina:      "f91e4d78c2a65f7423a5997973871eec96a285c4b3264c03b9e10284785f6bfc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0a5e0db99259ef1bdef858e5e053fc3a8af433d261bba0072bbe44b43c8636c"
+    root_url "https://github.com/Tenzer/homebrew-tap/releases/download/tfk8s-0.1.5"
+    sha256 cellar: :any_skip_relocation, catalina:     "7c13860c009760199fd8c8106a1c6099f574311e58efdddf12f877d33a6c653c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0a94e8dd972aad97f4c63818aa2c65209b2b44f110288e087426d51236c38217"
   end
 
   depends_on "go" => :build
