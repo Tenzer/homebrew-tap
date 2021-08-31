a7eff51796828e70161546fd366bd94caf6aed67
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-08-31 09:30:05 +0100

    Add ARM64 macOS bottle for tfk8s

--- Formula/tfk8s.rb
+++ Formula/tfk8s.rb
@@ -7,8 +7,9 @@ class Tfk8s < Formula
 
   bottle do
     root_url "https://github.com/Tenzer/homebrew-tap/releases/download/tfk8s-0.1.7"
-    sha256 cellar: :any_skip_relocation, catalina:     "02c4a0b9e11e6c816a18342c425a0e49e0c0d45f521cf75bb49129d24c047058"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f36cf7de4b7decaa6aa1b0f40ec7476dc178276fe23047a8eb4e27efc0812ca1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "4de1b3bcaad739c887937a177355657657b2df7ece8ee887b3884461e0796925"
+    sha256 cellar: :any_skip_relocation, catalina:      "02c4a0b9e11e6c816a18342c425a0e49e0c0d45f521cf75bb49129d24c047058"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f36cf7de4b7decaa6aa1b0f40ec7476dc178276fe23047a8eb4e27efc0812ca1"
   end
 
   depends_on "go" => :build
